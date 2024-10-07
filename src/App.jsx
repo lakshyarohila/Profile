721ad94e189498bee0ba1d43f959cc1d03ef8802
lakshyarohila <lakshyarohila99@gmail.com>
2024-10-07 23:40:13 +0530

    icons-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import bg from "./svg.svg";
+import c from './c.svg';
+import cc from './cc.svg';
+import html5 from './html5.svg';
+import css from './css.svg';
+import node from './node-js.svg';
+import js from './javascript.svg';
+import tail from './tailwind-css.svg';
 function App() {
   return (
     <>
@@ -21,31 +28,81 @@ function App() {
             </h1>
 
             <div className="daddy space-y-6 flex h-40 flex-col w-full mt-20">
+              <a href="https://learnhub1406.netlify.app/">
+                <div className="box w-full flex items-center pl-4 ">
+                  <div className="name w-32 text-white text-2xl font-semibold border-r my-1">
+                    <h1 className="cursor-pointer">Learn Hub</h1>
+                  </div>
+
+                  <h1 className="pl-4 bg-gray-400/10 backdrop-blur-md  text-white font-medium text-lg ml-2 px-2 rounded-full">
+                    {" "}
+                    A Platfrom For Learning Code & New Technologys
+                  </h1>
+                </div>
+              </a>
               <div className="box w-full flex items-center pl-4 ">
                 <div className="name w-32 text-white text-2xl font-semibold border-r my-1">
-                  <h1>Learn Hub</h1>
+                  <h1 className="cursor-pointer">Tars</h1>
                 </div>
-                <h1 className="pl-4"> hello</h1>
+                <h1 className="pl-4  bg-gray-400/10 backdrop-blur-md  text-white font-medium text-lg ml-2 px-2 rounded-full">
+                  {" "}
+                  Web App Like Chat-GPT - Using Open-Ai{" "}
+                </h1>
               </div>
               <div className="box w-full flex items-center pl-4 ">
                 <div className="name w-32 text-white text-2xl font-semibold border-r my-1">
-                  <h1>Learn Hub</h1>
+                  <h1 className="cursor-pointer">Learn Hub</h1>
                 </div>
-                <h1 className="pl-4"> hello</h1>
+                <h1 className="pl-4  bg-gray-400/10 backdrop-blur-md  text-white font-medium text-lg ml-2 px-2 rounded-full">
+                  {" "}
+                  hello
+                </h1>
               </div>
               <div className="box w-full flex items-center pl-4 ">
                 <div className="name w-32 text-white text-2xl font-semibold border-r my-1">
-                  <h1>Learn Hub</h1>
+                  <h1 className="cursor-pointer">Learn Hub</h1>
                 </div>
-                <h1 className="pl-4"> hello</h1>
-              </div>
-              <div className="box w-full flex items-center pl-4 ">
-                <div className="name w-32 text-white text-2xl font-semibold border-r my-1">
-                  <h1>Learn Hub</h1>
-                </div>
-                <h1 className="pl-4"> hello</h1>
+                <h1 className="pl-4  bg-gray-400/10 backdrop-blur-md  text-white font-medium text-lg ml-2 px-2 rounded-full">
+                  {" "}
+                  hello
+                </h1>
               </div>
             </div>
+          </section>
+
+          <section className="mt-32 flex flex-col w-full">
+            <h1 className="text-3xl ml-16 font-medium mb-4 w-fit text-center bg-white rounded-full px-4 py-1">Technologys -Skills</h1>
+            <div className="daddy grid grid-cols-4 space-y-5 mt-6  ml-8">
+              <div className="icon">
+                <img className="w-20 h-20" src={c} alt="" />
+                <h1 className="text-white pl-8 pt-2">C</h1>
+              </div>
+              <div className="icon">
+                <img className="w-20 h-20" src={cc} alt="" />
+                <h1 className="text-white pl-7 pt-2">C++</h1>
+              </div>
+              <div className="icon">
+                <img className="w-20 h-20" src={html5} alt="" />
+                <h1 className="text-white pl-6 pt-2">HTML</h1>
+              </div>
+              <div className="icon">
+                <img className="w-20 h-20" src={css} alt="" />
+                <h1 className="text-white pl-6 pt-2">CSS</h1>
+              </div>
+              <div className="icon">
+                <img className="w-20 h-20" src={node} alt="" />
+                <h1 className="text-white pl-3 pt-2">Node.Js</h1>
+              </div>
+              <div className="icon">
+                <img className="w-20 h-20" src={js} alt="" />
+                <h1 className="text-white pl-6 pt-2">JavaScript</h1>
+              </div>
+              <div className="icon">
+                <img className="w-20 h-20" src={tail} alt="" />
+                <h1 className="text-white pl-1 pt-2">Tailwind-Css</h1>
+              </div>
+            </div>
+
           </section>
         </div>
       </main>
